4fafbdfd21185a0aa6f6bda4135db64bbdfd72ba
amirrezaask <raskarpour@gmail.com>
2021-04-19 00:17:50 +0430

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# spawn.nvim
+An standard library for me to help me with my plugins
+
+# Goals
+- Easy but powerful libuv.spawn, probably with coroutines
---
+++ lua/spawn/init.lua
@@ -0,0 +1,73 @@
+local function exists(tbl, keys)
+  for _, k in pairs(keys) do
+    if not tbl[k] then
+      return k
+    end
+  end
+  return nil
+end
+
+local function spawn(opts)
+  if not opts then
+    print('Please pass a table with something in it, i dont know what to do')
+  end
+  local keys_missing = exists(opts, { 'command' })
+  if keys_missing then
+    print('key ' .. keys_missing(' is missing.'))
+  end
+  local uv = vim.loop
+  local stdin = uv.new_pipe(false)
+  local stdout = uv.new_pipe(false)
+  local stderr = uv.new_pipe(false)
+  if opts.stdin then
+    if type(opts.stdin) == 'table' then
+      uv.write(stdin, table.concat(opts.stdin))
+    end
+    if type(opts.stdin) == 'string' then
+      uv.write(stdin, table.concat(opts.stdin))
+    end
+  end
+  local handle, _ =
+    uv.spawn(opts.command, { args = opts.args, stdio = { stdin, stdout, stderr } }, function(code, signal)
+      assert(code == 0, 'process ' .. opts.command .. ' exited with code ' .. code)
+      if opts.on_exit and type(opts.on_exit) == 'function' then
+        opts.on_exit()
+      end
+    end)
+  if opts.stdout then
+    if type(opts.stdout) == 'function' then
+      uv.read_start(stdout, function(err, data)
+        assert(not err, 'error in reading from stdout')
+        -- TODO: maybe split this by newline
+        if data then
+          opts.stdout(vim.split(data, '\n'))
+        end
+      end)
+    end
+    if type(opts.stdout) == 'number' then
+      uv.read_start(stdout, function(err, data)
+        assert(not err, 'error in reading from stdout')
+        if data then
+          vim.schedule(function()
+            vim.api.nvim_buf_set_lines(opts.stdout, 0, -1, false, vim.split(data, '\n'))
+          end)
+        end
+      end)
+    end
+  end
+  uv.shutdown(stdin, function()
+    uv.close(handle, function()
+    end)
+  end)
+end
+
+spawn({
+  command = 'ls',
+  args = { '-al', '/' },
+  stdout = 6,
+  on_exit = function()
+    print('on_exit callback')
+  end,
+})
+
+return spawn
