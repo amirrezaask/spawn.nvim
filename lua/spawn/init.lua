9d446c6ec5bd50f2bf6bb96927b4c57725e73904
amirrezaask <raskarpour@gmail.com>
2021-05-18 20:51:32 +0430

    use vim jobstart

--- lua/spawn/init.lua
+++ lua/spawn/init.lua
@@ -1,141 +1,125 @@
-local function exists(tbl, keys)
-  for _, k in pairs(keys) do
-    if not tbl[k] then
-      return k
-    end
-  end
-  return nil
-end
-
-local function close(pipe)
-  if not pipe:is_closing() then 
-    pipe:close()
-  end
-end
 local function spawn(opts)
-  if not opts then
-    print('Please pass a table with something in it, i dont know what to do')
-  end
-  local keys_missing = exists(opts, { 'command' })
-  if keys_missing then
-    print('key ' .. keys_missing(' is missing.'))
-  end
-  local uv = vim.loop
-  local stdin = nil
-  if opts.stdin then
-    stdin = uv.new_pipe(false)
-  end
-  local stdout = uv.new_pipe(false)
-  local stderr = uv.new_pipe(false)
-
-  if opts.sync and opts.stdout then
-    print('you can specify only one of stdout or sync')
+  assert(opts, 'You should pass opts')
+  assert(opts.command, 'You should pass opts.command')
+  if opts.stdout and opts.sync then
+    assert(false, 'cannot specify both stdout and sync')
     return
   end
+  local stdout_done = false
+  local stderr_done = false
+  local process_done = false
+  local stdout = {}
+  local stderr = {}
 
-  local done = false
-  local handle, _ = uv.spawn(opts.command, {
-    args = opts.args,
-    stdio = { stdin, stdout, stderr },
-  }, function(code, _)
-    -- assert(code == 0, 'process ' .. opts.command .. ' exited with code ' .. code)
-    stdin:close()
-    if opts.on_exit and type(opts.on_exit) == 'function' then
-      opts.on_exit()
-    end
-  end)
-  if not handle then
-    print('could not spawn the process')
-    return
-  end
-  if opts.stdin then
-    if type(opts.stdin) == 'table' then
-      uv.write(stdin, table.concat(opts.stdin, '\n'), function()
-        close(stdin)
-      end)
-    end
-    if type(opts.stdin) == 'string' then
-      uv.write(stdin, opts.stdin, function()
-        close(stdin)
-      end)
+
+  local function is_eof_stdout(data)
+    if data[1] == '' then
+      stdout_done = true
     end
   end
-  uv.read_start(stderr, function(err, data)
-    assert(not err, 'cannot read from stderr')
-    if data then
-      print('err: ' .. data)
-    end
-    if data == nil then
-      close(stderr)
-    end
-  end)
-  if opts.stdout then
-    if type(opts.stdout) == 'function' then
-      uv.read_start(stdout, function(err, data)
-        assert(not err, 'error in reading from stdout')
-        -- TODO: maybe split this by newline
-        if data then
-          opts.stdout(vim.split(data, '\n'))
-        end
-        if data == nil then
-          close(stdout)
-        end
-      end)
-    end
 
-    if type(opts.stdout) == 'number' then
-      uv.read_start(stdout, function(err, data)
-        assert(not err, 'error in reading from stdout')
-        if data then
-          vim.schedule(function()
-            vim.api.nvim_buf_set_lines(opts.stdout, 0, -1, false, vim.split(data, '\n'))
-          end)
-        end
-        if data == nil then
-          close(stdout)
-        end
-      end)
+  local function is_eof_stderr(data)
+    if data[1] == '' then
+      stderr_done = true
     end
+  end
+
+  local function on_stdout_buffer(buf)
+    return function(_, data, _)
+      if is_eof_stdout(data) then
+        stdout_done = true
+        return
+      end
+      local c = vim.api.nvim_buf_get_lines(buf)
+      vim.api.nvim_buf_set_lines(buf, c, -1, false, data)
+    end
+  end
+
+  local function on_stderr_buffer(buf)
+    return function(_, data, _)
+      if is_eof_stderr(data) then
+        stderr_done = true
+        return
+      end
+      local c = vim.api.nvim_buf_get_lines(buf)
+      vim.api.nvim_buf_set_lines(buf, c, -1, false, data)
+
+    end
+  end
+
+  if type(opts.stdout) == "number" then
+    opts.stdout = on_stdout_buffer(opts.stdout)
+  end
+
+  if type(opts.stderr) == "number" then
+    opts.stdout = on_stderr_buffer(opts.stderr)
+  end
+
+  local function on_stdout_sync()
+    return function(_, data,_ )
+      if is_eof_stdout(data) then
+        stdout_done = true
+      end
+      for _, l in ipairs(data) do
+        if l ~= '' then
+          table.insert(stdout, l)
+        end
+      end
+    end
+  end
+
+  local function on_stderr_sync()
+    return function(_, data,_ )
+      if is_eof_stderr(data) then
+        stderr_done = true
+      end
+      for _, l in ipairs(data) do
+        if l ~= '' then
+          table.insert(stderr, l)
+        end
+      end
+    end
+  end
+
+  local function on_exit(id, code, event)
+    if code ~= 0 then 
+      success = false
+    end
+    process_done = true
+    if opts.on_exit then opts.on_exit(id, code, event) end
+  end
+
+  local on_stdout
+  local on_stderr
+
+  if type(opts.stdout) == 'number' then
+    on_stdout = on_stdout_buffer(opts.stdout)
+  end
+
+  if type(opts.stderr) == 'number' then
+    on_stderr = on_stdout_buffer(opts.stderr)
   end
 
   if opts.sync then
-    opts.sync.interval = opts.sync.interval or 10
-    opts.sync.timeout = opts.sync.timeout or 1000
-    local output = {}
-    uv.read_start(stdout, function(err, data)
-      assert(not err, 'error in reading from stdout: ')
-      if data then
-        local tmp = vim.split(data, '\n')
-        for _, v in pairs(tmp) do
-          if v ~= '' then
-            table.insert(output, v)
-          end
-        end
-      end
-      if data == nil then
-        close(stdout)
-        done = true
-      end
-    end)
-    vim.wait(opts.sync.timeout, function()
-      return done
-    end, opts.sync.interval)
-    close(stdin)
-    close(stderr)
-    close(stdout)
+    on_stdout = on_stdout_sync()
+    on_stderr = on_stderr_sync()
+  end
 
-    stdin = nil
-    stdout = nil
-    stderr = nil
-    return output
+  local success = true
+
+  local stdin_id = vim.fn.jobstart(opts.command, {
+    cwd = opts.cwd or '.',
+    on_exit = on_exit,
+    on_stdout = on_stdout,
+    on_stderr = on_stderr
+  })
+  if type(opts.stdin) == 'table' or type(opts.stdin) == 'string' then
+    vim.fn.chansend(stdin_id, opts.stdin)
+  end
+  if opts.sync then
+    vim.wait(2000, function()
+      return process_done and stdout_done and stderr_done 
+    end, 10)
+    return stdout, stderr, success
   end
 end
-
--- P(spawn({
---   command = 'bash',
---   args = { 'lazy.sh' },
---   -- stdout = 9,
---   sync = { timeout = 4000, interval = 10 },
--- }))
-
-return spawn
